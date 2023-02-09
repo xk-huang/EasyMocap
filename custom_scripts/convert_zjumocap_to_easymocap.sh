0c5c18d1123b2ccd642e2428fefad8e4922b8ae9
xiaoke@ivgsz_171 <xiaokehuang@foxmail.com>
2023-02-09 21:02:44 +0800

    custom scripts for re-estimate zjumocap 6 fix soft link creation

--- custom_scripts/convert_zjumocap_to_easymocap.sh
+++ custom_scripts/convert_zjumocap_to_easymocap.sh
@@ -6,7 +6,7 @@ cd ${data}
 mkdir -p olds
 mv mask mask_cihp new_params new_vertices params vertices annots_python2.npy annots.npy match_info.json *.mp4 *.py *.npy annots.json olds/
 
-if [[ -d keypoints2d ]]; then
+if [[ -d keypoints2d ]] && [[ ! -d openpose ]]; then
     ln -s keypoints2d openpose
 fi
 mv annots olds/
