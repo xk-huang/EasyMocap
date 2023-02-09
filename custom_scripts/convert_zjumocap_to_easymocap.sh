8f7057d3dd98d8dcef95e0d00d6a5c0e6a4f3a4c
xiaoke@ivgsz_171 <xiaokehuang@foxmail.com>
2023-02-09 16:31:37 +0800

    custom scripts for re-estimate zjumocap 4
    handle 313 and 315
    handle file and dir existence

--- custom_scripts/convert_zjumocap_to_easymocap.sh
+++ custom_scripts/convert_zjumocap_to_easymocap.sh
@@ -6,12 +6,73 @@ cd ${data}
 mkdir -p olds
 mv mask mask_cihp new_params new_vertices params vertices annots_python2.npy annots.npy match_info.json *.mp4 *.py *.npy annots.json olds/
 
-if [[ ! -d keypoints2d ]]; then
+if [[ -d keypoints2d ]]; then
     ln -s keypoints2d openpose
 fi
 mv annots olds/
 
 mkdir images
 mv Camera* images
+
+# Special case for CoreView_313 and CoreView_315
+
+relink_dir() {
+    target_dir=$1
+    echo "relinking: $target_dir"
+
+    if [[ ! -d "${target_dir}" ]] && [[ ! -d olds/"${target_dir}" ]]; then
+        echo "ERROR: neither ${target_dir} nor olds/${target_dir} exist"
+        exit 1
+    elif [[ ! -d "${target_dir}" ]]; then
+        mkdir -p "${target_dir}"
+
+    elif [[ ! -d olds/"${target_dir}" ]]; then
+        mv -i  -- "${target_dir}"/ olds/
+        mkdir -p "${target_dir}"
+    fi
+
+    find olds/"${target_dir}" -maxdepth 1 -mindepth 1 -type d | sort | while iFS= read -r full_sub; do
+        sub=$(basename "$full_sub")
+        mkdir -p "${target_dir}"/"$sub"
+        echo "processing: $full_sub -> ${target_dir}/$sub" 
+
+        cnt=0
+        for file in $(ls "$full_sub" | sort); do
+            # mv -i -- $sub/$file $sub/$(printf %06d cnt).jpg"$extension"
+            extension="${file##*.}"
+            if [[ ! -f ./"${target_dir}"/"$sub"/$(printf %06d $cnt)."$extension" ]]; then
+                ln -s ../../"$full_sub"/"$file"  ./"${target_dir}"/"$sub"/$(printf %06d $cnt)."$extension"
+            # else
+            #     md5_before=$(md5sum ./"${target_dir}"/"$sub"/$(printf %06d $cnt)."$extension" | awk '{print $1}')
+            #     md5_after=$(md5sum "$full_sub"/"$file" | awk '{print $1}')
+            #     if [[ $md5_before != $md5_after ]]; then
+            #         echo "ERROR: md5sum of ${target_dir} before and after is not the same"
+            #         echo "before: " $md5_before
+            #         echo "after: " $md5_after
+            #         exit 1
+            #     fi
+            fi
+            cnt=$(( cnt + 1 ))
+        done 
+
+        num_files_before=$(ls ./"${target_dir}"/"$sub" | wc -l)
+        num_files_after=$(ls "$full_sub" | wc -l)
+        if [[ $num_files_before -ne $num_files_after ]]; then
+            echo "ERROR: number of files before and after is not the same"
+            echo "before: " $num_files_before
+            echo "after: " $num_files_after
+            exit 1
+        else
+            echo "number of files before and after is the same: " $num_files_before
+        fi
+    done
+}
+
+if [[ "$(basename ${data})" = "CoreView_313" ]] || [[ "$(basename ${data})" = "CoreView_315" ]]; then
+    echo $data is CoreView_313 or CoreView_315, the "extri.yml" and "intr.yml" are missing, the images, keypoints2d are not named after numbers
+    relink_dir images
+    relink_dir keypoints2d
+
+fi
 
 exit 0
--- custom_scripts/ln_smpl_to_smplh.sh
+++ custom_scripts/ln_smpl_to_smplh.sh
@@ -3,9 +3,14 @@ set -e
 base_dir=data/zju_mocap/zju_mocap_easymocap_smpl
 target_dir=data/zju_mocap/zju_mocap_easymocap_smplh
 
-for dir in `ls $base_dir -U`; do
+for dir in `ls $base_dir`; do
     mkdir -p $target_dir/$dir
-    ln -s "../../../../$base_dir/$dir/images/" "$target_dir/$dir/images"
-    ln -s "../../../../$base_dir/$dir/extri.yml" "$target_dir/$dir/extri.yml"
-    ln -s "../../../../$base_dir/$dir/intri.yml" "$target_dir/$dir/intri.yml"
+    for target_name in images extri.yml intri.yml; do
+        if [[ ! -f "$target_dir/$dir/$target_name" ]] && [[ ! -d "$target_dir/$dir/$target_name" ]] ; then
+            echo "ln -s ../../../../$base_dir/$dir/$target_name $target_dir/$dir/$target_name"
+            ln -s "../../../../$base_dir/$dir/$target_name" "$target_dir/$dir/$target_name"
+        else
+            echo "$target_dir/$dir/$target_name already exists"
+        fi
+    done
 done
